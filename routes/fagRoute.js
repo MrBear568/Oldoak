3df6cf36bda8ce8e7c3e7d1ea993aa13343a8dce
JesperVH <jesper9898@gmail.com>
2020-11-20 11:08:55 +0100

    jep

--- routes/fagRoute.js
+++ routes/fagRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+let text = {overskrift: 'Dette er en overskrift', brødtext: 'Dette er en brødtext'}
+
 router.get('/fag', (req, res) =>{
-    res.render('fag');
+    res.render('fag', texts);
 })
 
 module.exports = router;
