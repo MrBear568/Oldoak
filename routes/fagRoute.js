b31ad61332f08bebd9829cf7ed6812db1321cc83
JesperVH <jesper9898@gmail.com>
2020-11-20 12:35:14 +0100

    yeh

--- routes/fagRoute.js
+++ routes/fagRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs').promises
+
+
 let text = [{overskrift: 'Dette er en overskrift', brødtext: 'Dette er en brødtext'}, {overskrift: 'Dette er en overskrift', brødtext: 'Dette er en brødtext'}]
 
-router.get('/fag', (req, res) =>{
-    res.render('fag', {texts: text});
+router.get('/fag', async (req, res) =>{
+    let filnavne = await fs.readdir(__dirname + "/filer")
+    res.render('fag', {texts: text, fil: filnavne});
 })
+
 
 module.exports = router;
