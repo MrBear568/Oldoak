cd2334fe0315eb43cd7a0e9630040b47b7aaa773
Bjoern Petersen Werner <bjrn.werner@gmail.com>
2020-11-20 12:24:45 +0100

    pics and stuff

--- routes/omMigRoute.js
+++ routes/omMigRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs').promises;
 
-router.get('/omMig', (req, res) =>{
-    res.render('omMig');
+router.get('/omMig', async (req, res) =>{
+    let pics = await fs.readdir(__dirname + '../pictures')
+    res.render('omMig', {pics: pics});
 })
 
 module.exports = router;
