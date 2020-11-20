986b72d8f347ecd217394a42d7e3d4dcbc3d1f14
Bjoern Petersen Werner <bjrn.werner@gmail.com>
2020-11-20 10:10:45 +0100

    app kode indtastet

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    autoIndex: true, 
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to mongoose'))
+
+//Routes 
+const jokesRoute = require('./routes/jokes');
+app.use('/', jokesRoute);
+
+let server = app.listen(process.env.PORT || 8080, () => {
+    let port = server.address().port;
+    console.log(`server kører på port ${port}`);
+})
