414508efc8fb09abc757a1a1888b40b44fd3cf7d
JesperVH <jesper9898@gmail.com>
2020-11-23 12:10:04 +0100

    faq done

--- routes/fagRoute.js
+++ routes/fagRoute.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const fs = require('fs').promises
 
-
-let text = [{overskrift: 'Dette er en overskrift', brødtext: 'Dette er en brødtext'}, {overskrift: 'Dette er en overskrift', brødtext: 'Dette er en brødtext'}]
+let text = [{overskrift: 'DO YOU SPEAK/ UNDERSTAND DANISH?', txt: 'I understand most of Danish and can speak little as well, so feel free to communicate in Danish if thats the easiest for you.'}, 
+{overskrift: ' WHERE CAN I SEE YOUR PRICES?', txt: 'I offer 4 wedding packages: Ceremony & Portraits (2 hours) 5.000DKK Half Day (5 hours) 10.000DKK Whole Day (10 hours) 16.000DKK Extended Day (13 hours) 18.000DKK +Transport Expenses for all.'}, 
+{overskrift: 'HOW LONG DO YOU TAKE TO EDIT THE PHOTOS?', txt: 'I usually take 5-7 weeks to edit the photos. Sometimes it takes little less and sometimes a bit more, it depends on how busy the season is. Between quality and deadlines I will always choose the first one. I will always put all the love and attention into my work, because the photographs I create will last forever.'}]
 
 router.get('/fag', async (req, res) =>{
-    let filnavne = await fs.readdir(__dirname + "/filer")
-    res.render('fag', {texts: text, fil: filnavne});
+    res.render('fag', {texts: text});
 })
 
 
