223e1f8fd444ac7a713716a1929b9ee3b3639ca7
JesperVH <jesper9898@gmail.com>
2020-11-25 11:28:24 +0100

    lort

--- routes/galleri.js
+++ routes/galleri.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs').promises
 
+
+
 router.get('/galleri', async (req, res) =>{
-    let filnavne = await fs.readdir(__dirname + "/pictures/galleri")
-    res.render('galleri', {billede: filnavne});
+    let billeder = require('../public/pictures/galleri');
+    res.render('galleri', {billede: billeder});
 })
 
 
