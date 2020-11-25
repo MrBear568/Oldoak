9c59b34a920c57af58c569187d62a41ad4e81cb2
Bjoern Petersen Werner <bjrn.werner@gmail.com>
2020-11-25 18:36:10 +0100

    forside-reouter ændringer

--- routes/forsideRoute.js
+++ routes/forsideRoute.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs').promises;
 
+router.get('/', async (req, res) =>{
+    try {
+        res.redirect('/home')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 router.get('/home', async (req, res) =>{
     res.render('forside');
 })
