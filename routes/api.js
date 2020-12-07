4f3680cd9750b45cbee8fd330a464af91be6b0e3
Sebastian Rom <eaasrvh@students.eaaa.dk>
2020-12-07 14:25:17 +0100

    Ændringer til heruko

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/kontakt');
 
-router.get('/', (req, res) => {
-
-})
-
 router.get('/api/requests', async (req, res) => {
     try {
         const requests = await controller.hentRequests();
--- scripts/kalender.js
+++ scripts/kalender.js
@@ -21,18 +21,18 @@ document.addEventListener('DOMContentLoaded', async function () {
         weekNumbers: true,
         showNonCurrentDates: false,
         dateClick: async function (info) {
-        let requests = await fetch('http://localhost:8080/api/requests');
-        let jsondata = await requests.json();
-        for (d of jsondata) {
-            if (d.dato.slice(0, 10) === info.dateStr && d.status === true) { 
-                alert('OBS: Datoen er ikke tilgængelig')
-                window.location.reload()
-            } else {
-                let valgtDato = info.dateStr;
-                let datoTA = document.getElementById('dato');
-                datoTA.value = valgtDato;
+            let requests = await fetch('https://oldoak-weddings.herokuapp.com/api/requests');
+            let jsondata = await requests.json();
+            for (d of jsondata) {
+                if (d.dato.slice(0, 10) === info.dateStr && d.status === true) {
+                    alert('OBS: Datoen er ikke tilgængelig')
+                    window.location.reload()
+                } else {
+                    let valgtDato = info.dateStr;
+                    let datoTA = document.getElementById('dato');
+                    datoTA.value = valgtDato;
                 }
-            }    
+            }
         },
     });
     calendar.render();
