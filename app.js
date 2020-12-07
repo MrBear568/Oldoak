70739786bec18e4d849aac9e8a9a66b7120aee5f
Sebastian Rom <eaasrvh@students.eaaa.dk>
2020-12-07 14:40:24 +0100

    Pakkeløsninger til DB

--- app.js
+++ app.js
@@ -67,11 +67,11 @@ app.use('/', loginRoute);
 //controller.opretLogin('admin', 'nimda')
 
 //oprettelse af pakkeløsning
-// const controller = require('./controllers/pakkeløsning')
-// controller.opretPakkeløsning('Ceremoni og portrætter', '- 2 timer', '- 5.000,- DKK', '- 2500,- DKK pr. time', '- Den bedste pakke at vælge hvis du ønsker billeder til din forlovelse, rådhus vielse, eller hvis du kun vil have det helt vigtige med fra brylluppet - jeres vielse plus jeres billede af jeres nye kærlighed.')
-// controller.opretPakkeløsning('Halv dag', '- 5 timer', '- 10.000,- DKK', '- 2000,- DKK pr. time', '- Denne pakke blive som regel valgt af par som ønsker at jeg skal være en del af bryllupsceremonien, receptionen og selvfølgelig også få nogle smukke bryllups portrætter. Når det er sagt, vælger i selvfølgelig hvad der skal sættes fokus på, og hvad der skal fotograferes.')
-// controller.opretPakkeløsning('Hel dag', '- 10 timer', '- 16.000,- DKK', '- 1600,- DKK pr. time', '- I denne pakker kommer jeg som regel til ceremonien og smutter om natten efter den første dans. I løbet af dagen fanger jeg alle smil, kram og glade øjeblikke. Måske vil i dog have at jeg kommer tidligt i stedet for, og ser én, eller begge af jer blive klar om morgenen Eller måske vil i have noget midt i mellem, det kan tilpasses efter jeres behov.')
-// controller.opretPakkeløsning('Forlænget dag', '- 13 timer', '- 18.000,- DKK', '- ca. 1384,- DKK pr. time', '- Den helt store pakke. Jeg følger jer i alle øjeblikke, og sikrer mig at jeg ikke misser noget. Fra tidlig morgen til midnat, fra middag til sen nat, eller noget tredje midt i mellem, i vælger tidspunktet jeg begynder, og slutter, inden for tidsrammen.')
+const controller = require('./controllers/pakkeløsning')
+controller.opretPakkeløsning('Ceremoni og portrætter', '- 2 timer', '- 5.000,- DKK', '- 2500,- DKK pr. time', '- Den bedste pakke at vælge hvis du ønsker billeder til din forlovelse, rådhus vielse, eller hvis du kun vil have det helt vigtige med fra brylluppet - jeres vielse plus jeres billede af jeres nye kærlighed.')
+controller.opretPakkeløsning('Halv dag', '- 5 timer', '- 10.000,- DKK', '- 2000,- DKK pr. time', '- Denne pakke blive som regel valgt af par som ønsker at jeg skal være en del af bryllupsceremonien, receptionen og selvfølgelig også få nogle smukke bryllups portrætter. Når det er sagt, vælger i selvfølgelig hvad der skal sættes fokus på, og hvad der skal fotograferes.')
+controller.opretPakkeløsning('Hel dag', '- 10 timer', '- 16.000,- DKK', '- 1600,- DKK pr. time', '- I denne pakker kommer jeg som regel til ceremonien og smutter om natten efter den første dans. I løbet af dagen fanger jeg alle smil, kram og glade øjeblikke. Måske vil i dog have at jeg kommer tidligt i stedet for, og ser én, eller begge af jer blive klar om morgenen Eller måske vil i have noget midt i mellem, det kan tilpasses efter jeres behov.')
+controller.opretPakkeløsning('Forlænget dag', '- 13 timer', '- 18.000,- DKK', '- ca. 1384,- DKK pr. time', '- Den helt store pakke. Jeg følger jer i alle øjeblikke, og sikrer mig at jeg ikke misser noget. Fra tidlig morgen til midnat, fra middag til sen nat, eller noget tredje midt i mellem, i vælger tidspunktet jeg begynder, og slutter, inden for tidsrammen.')
 
 
 
